6e1eb4726a4c5cc202aa4792f78afdbcaa9ba21e
yaakovizel <z7685727@gmail.com>
2026-07-08 17:08:37 +0300

    read and wriye from json

---
+++ services/file_service.js
@@ -0,0 +1,14 @@
+import fs from "fs/promises";
+
+export async function readJson(filePath) {
+  const fileContent = await fs.readFile(filePath, "utf-8");
+  if (!fileContent.trim()) {
+    return [];
+  }
+  return JSON.parse(fileContent);
+}
+
+export async function writeJson(filePath, content) {
+  const stringContent = JSON.stringify(content);
+  await fs.writeFile(filePath, stringContent);
+}
